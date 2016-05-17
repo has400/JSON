e3eaeadaa147730b081f7d971e14562d6ec3fa51
has400 <has400@live.com.au>
2016-05-17 22:29:10 +1000

    JSON

--- app/src/main/java/com/example/kripzy/url/MainActivity.java
+++ app/src/main/java/com/example/kripzy/url/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.kripzy.url;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Connection;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -19,10 +23,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Key;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvData;
+    private ImageView imgtest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,72 +36,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvData = (TextView) findViewById(R.id.tvJsonItem);
+        imgtest = (ImageView) findViewById(R.id.imageView);
 
 
 
-    new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+        new JSONTask().execute("http://api.champion.gg/champion/Ekko/");
     }
 
 
-    public class JSONTask extends AsyncTask<String,String, String>{
-    @Override
-    protected String doInBackground(String ... params) {
-        HttpURLConnection connection = null;
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(params[0]);
+    public class JSONTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
 
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            InputStream stream = connection.getInputStream();
-
-            reader = new BufferedReader(new InputStreamReader(stream));
-
-            StringBuffer buffer = new StringBuffer();
-
-            String line = "";
-
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line);
-            }
-
-            String finalJson = buffer.toString();
-
-            JSONObject parentObject = new JSONObject(finalJson);
-            JSONArray parentArray = parentObject.getJSONArray("movies");
-
-            JSONObject finalObject = parentArray.getJSONObject(0);
-
-            String ChampionName = finalObject.getString("movie");
-            String mostGames = finalObject.getString("year");
-
-            return ChampionName + mostGames;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
             try {
-                if (reader != null) {
-                    reader.close();
+                URL url = new URL(params[0]);
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
                 }
+
+                String finalJson = buffer.toString();
+
+                JSONArray jsonarray = new JSONArray(finalJson);
+
+                for (int i = 0; i < jsonarray.length(); i++) {
+
+                    JSONObject finalObject = jsonarray.getJSONObject(i);
+
+                    String ChampionName = finalObject.getString("key");
+                    String mostGames = finalObject.getString("role");
+
+
+
+
+
+                    return ChampionName + mostGames;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+            }
+            return null;
         }
-    return null;
-    }
+
         @Override
-                protected void onPostExecute(String result){
+        protected void onPostExecute(String result) {
             super.onPostExecute(result);
             tvData.setText(result);
+
+
         }
 
     }
